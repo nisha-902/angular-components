c9e9220ffbca35fffa1d908fb227eada1f556d09
Nisha <nk1655550@gmail.com>
2021-03-17 11:01:01 -0400

    components in angular

---
+++ src/app/background/background.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-background',
+  templateUrl: './background.component.html',
+  styleUrls: ['./background.component.css']
+})
+export class BackgroundComponent {
+
+
+  facebook="./assets/facebook.png"
+  tw="./assets/tw.png"
+  wifi="./assets/wifi.png"
+  google="./assets/google.jpg"
+  home="./assets/home.png"
+  hello="./assets/hello.jpg"
+  tree="./assets/tree.jpg"
+  sea1="./assets/sea.jpg"
+  pariss="./assets/paris.jpg"
+  house="./assets/house.jpg"
+  boat="./assets/boat.jpg"
+  wine="./assets/wine.jpg"
+  beech="./assets/beech.jpg"
+  tour="./assets/tour.jpg"
+  ballon="./assets/ballon.jpg"
+  icon="./assets/icon.png"
+
+
+}
